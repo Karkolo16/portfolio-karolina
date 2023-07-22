1425e401c07798446badb79368c552e3cc3c475f
Karolina <ka.kolomyjec@gmail.com>
2023-07-22 10:00:21 +0200

    Fourth commit: Add styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 const Header = () => {
+  const containerStyle = {
+    textAlign: "center",
+  };
+
+  const headerStyle = {
+    fontFamily: "Lumanosimo, sans-serif",
+    color: "white",
+    marginBottom: "-20px",
+  };
+  const subheaderStyle = {
+    fontFamily: "Noto Sans, sans-serif",
+    color: "gold",
+    fontSize: "21px",
+  };
+
   return (
-    <div>
-      <h1>Witaj na stronie głównej!</h1>
+    <div style={containerStyle}>
+      <h1 style={headerStyle}>Karolina Kołomyjec</h1>
+      <h2 style={subheaderStyle}>Junior Frontend/React Developer</h2>
     </div>
   );
 };
