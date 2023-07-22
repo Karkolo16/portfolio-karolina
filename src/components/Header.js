c2de160c213e520a1e5360b54cb4442f42908710
Karolina <ka.kolomyjec@gmail.com>
2023-07-22 10:10:04 +0200

    Fith commit: Change font in h1 in header and add line below h2 in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   };
 
   const headerStyle = {
-    fontFamily: "Lumanosimo, sans-serif",
+    fontFamily: "Oxygen, sans-serif",
     color: "white",
     marginBottom: "-20px",
   };
@@ -14,12 +14,28 @@ const Header = () => {
     fontFamily: "Noto Sans, sans-serif",
     color: "gold",
     fontSize: "21px",
+    position: "relative",
+  };
+
+  const subheaderLine = {
+    content: "",
+    display: "block",
+    width: "100%",
+    height: "1px",
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: "-5px",
+    left: 0,
+    borderRadius: "5px",
+    opacity: 0.4,
   };
 
   return (
     <div style={containerStyle}>
       <h1 style={headerStyle}>Karolina Kołomyjec</h1>
-      <h2 style={subheaderStyle}>Junior Frontend/React Developer</h2>
+      <h2 style={subheaderStyle}>
+        Junior Frontend/React Developer <span style={subheaderLine}></span>
+      </h2>
     </div>
   );
 };
