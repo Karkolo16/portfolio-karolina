7921a0269bf37cb2d022ebbffad498c9d53c33b2
Karolina <ka.kolomyjec@gmail.com>
2023-07-22 09:29:56 +0200

    Second commit: Add folder components, delete unnecessery files, add component HomePage

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Witaj na stronie głównej!</h1>
+      <p>3 fotki z portfolio.</p>
+    </div>
+  );
+};
+
+export default HomePage;
