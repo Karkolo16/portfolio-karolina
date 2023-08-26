eb7cb490c1cf554055b3884e838385e988ed3e9a
Karolina <ka.kolomyjec@gmail.com>
2023-08-26 10:46:15 +0200

    Seventh commit: change style to header in this sec tion and change display from row to column

--- src/components/MyProject.js
+++ src/components/MyProject.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./MyProject.css";
 
 const MyProject = () => {
+  const PortfolioStyle = {
+    fontFamily: "Oxygen, sans-serif",
+  };
+
   return (
     <div className="my-project">
+      <h3 style={PortfolioStyle} className="my-portfolio">
+        My Portfolio
+      </h3>
       <a href="https://karkolo16.github.io/ecomm/" target="_blank">
         <img src={require("../images/1.jpg")} alt="Ecomm" />
       </a>
