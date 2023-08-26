e9f29816c2877b111c4a79db6ca5a794ef4033f5
Karolina <ka.kolomyjec@gmail.com>
2023-08-26 10:56:18 +0200

    Eighth commit: add description to first project

--- src/components/MyProject.js
+++ src/components/MyProject.js
@@ -11,9 +11,14 @@ const MyProject = () => {
       <h3 style={PortfolioStyle} className="my-portfolio">
         My Portfolio
       </h3>
-      <a href="https://karkolo16.github.io/ecomm/" target="_blank">
-        <img src={require("../images/1.jpg")} alt="Ecomm" />
-      </a>
+      <div className="project">
+        <a href="https://karkolo16.github.io/ecomm/" target="_blank">
+          <img src={require("../images/1.jpg")} alt="Ecomm" />
+          <div style={PortfolioStyle} className="project-desc">
+            E-COMM STORE <p>Project of ecomm store made in BOOSTRAP5.</p>
+          </div>
+        </a>
+      </div>
       <a href="https://karkolo16.github.io/quote-of-the-day/" target="_blank">
         <img img src={require("../images/2.jpg")} alt="Quote Generator" />
       </a>
